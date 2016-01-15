495cca1972f408acbf2860ded34e54e67d6a12d0
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2016-01-15 17:41:41 +0700

    add utils

--- src/main/java/R/helper/Common.java
+++ src/main/java/R/helper/Common.java
@@ -1,12 +1,16 @@
 package R.helper;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.util.DisplayMetrics;
 
+import java.util.Calendar;
 import java.util.List;
+import java.util.TimeZone;
 
 /**
  * Created by duynk on 12/29/15.
@@ -72,4 +76,36 @@ public class Common {
         }
         return sb.toString();
     }
+
+    public static long getMillis() {
+        return Calendar.getInstance(TimeZone.getTimeZone("GMT")).getTimeInMillis();
+    }
+
+    public final static long ONE_SEC = 1000;
+    public final static long ONE_MINUTE = ONE_SEC*60;
+    public final static long HALF_HOUR = ONE_MINUTE*30;
+    public final static long ONE_HOUR = ONE_MINUTE*60;
+    public final static long HALF_DAY = ONE_HOUR*12;
+    public final static long ONE_DAY = ONE_HOUR*24;
+    public final static long ONE_WEEK = ONE_DAY*7;
+    public final static long ONE_WORKING_WEEK = ONE_DAY*5;
+    public final static long HALF_MONTH = ONE_DAY*15;
+    public final static long ONE_MONTH = ONE_DAY*30;
+
+    public static class Dialog {
+        public static void alert(final Context ctx, String message, final Callback callback) {
+            new AlertDialog.Builder(ctx)
+                    .setTitle("Alert")
+                    .setMessage(message)
+                    .setCancelable(false)
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            callback.onCompleted(ctx, new CallbackSuccess(null));
+                        }
+                    }).create().show();
+        }
+    }
+
+
 }
