ca746cc1f3b3ef1747217406d50e0941915390ff
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2016-01-21 16:15:58 +0700

    add r13

--- src/main/java/R/helper/Common.java
+++ src/main/java/R/helper/Common.java
@@ -111,4 +111,17 @@ public class Common {
     public static boolean isValidEmail(CharSequence target) {
         return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
     }
+
+    public static String r13(String input) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if       (c >= 'a' && c <= 'm') c += 13;
+            else if  (c >= 'A' && c <= 'M') c += 13;
+            else if  (c >= 'n' && c <= 'z') c -= 13;
+            else if  (c >= 'N' && c <= 'Z') c -= 13;
+            sb.append(c);
+        }
+        return sb.toString();
+    }
 }
