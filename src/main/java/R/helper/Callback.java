4c9dce0f3767033055c54bbb8c8c33b4d51e3dd6
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2016-01-26 14:28:36 +0700

    refactor callback

--- src/main/java/R/helper/Callback.java
+++ src/main/java/R/helper/Callback.java
@@ -5,6 +5,6 @@ import android.content.Context;
 /**
  * Created by duynk on 12/29/15.
  */
-public interface Callback {
-    void onCompleted(Context context, CallbackResult<?> result);
+public interface Callback<T> {
+    void onCompleted(Context context, CallbackResult<T> result);
 }
